82d384bf35735e86462c882e1ce7ab13d13a0a55
macra6 <macra1996@gmail.com>
2020-02-27 22:02:28 +0300

    1 EX

--- lesson11/homework.py
+++ lesson11/homework.py
@@ -5,12 +5,54 @@ n включительно.
 """
 
 
+# Генерирует числа Фибоначчи до какого-то числа
+class Fibonacci():
+    def __init__(self, amount):
+        self.counter = 0
+        self.amount = amount
+        self.current = 1
+        self.previous = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current >= self.amount:
+            raise StopIteration
+        else:
+            current = self.current
+            self.current += self.previous
+            self.previous = current
+            return current
+
+
+# Генерирует числа Фибоначчи до n-го числа
+class FibonacciNumber(Fibonacci):
+    def __next__(self):
+        if self.counter >= self.amount:
+            raise StopIteration
+        else:
+            self.counter += 1
+            current = self.current
+            self.current += self.previous
+            self.previous = current
+            return current
+
+print("1й итератор", end=": ")
+counter = Fibonacci(7)
+for i in counter:
+    print(i, end=" ")
+
+print("\n2й итератор", end=": ")
+counter1 = FibonacciNumber(7)
+for a in counter1:
+    print(a, end=" ")
+
 """
 2.
 Напишите класс, объектом которого будет итератор производящий только
 чётные числа до n включительно.
 """
-
 
 """
 3.
